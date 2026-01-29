1ecd5d5d35cd0f245f9b37b9cb22e74ad149fff7
jjarvela23 <168526631+jjarvela23@users.noreply.github.com>
2026-01-29 20:54:31 +0200

    nothing special

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -45,135 +45,36 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.toColor
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            nightModeToggle()
-        }
-    }
-}
-
-data class Message(val author: String, val body: String)
-
-@Composable
-fun MessageCard(msg: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)){
-        Column {
-            Spacer(modifier = Modifier.height(20.dp))
-            Image(
-                painter = painterResource(R.drawable.nature),
-                contentDescription = "finnish nature",
-
-                modifier = Modifier
-                    .size(40.dp)
-                    .clip(CircleShape)
-                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
-            )
-        }
-        Spacer(modifier = Modifier.width(8.dp))
-
-        var isExpanded by remember { mutableStateOf(false) }
-
-        val surfaceColor by animateColorAsState(
-            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
-        )
-
-        Column(modifier = Modifier.clickable {isExpanded = !isExpanded}) {
-            Spacer(modifier = Modifier.height(20.dp))
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.titleSmall,
-                fontFamily = FontFamily.Monospace
-
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                shadowElevation = 3.dp,
-                color = surfaceColor,
-                modifier = Modifier.animateContentSize().padding(1.dp)
-            ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.bodyLarge
-                )
-            }
+            NavigationSystem()
         }
     }
 }
 
 @Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(message)
+fun NavigationSystem() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Main_Screen", builder ={
+        composable("Main_Screen") {
+            MainScreen(navController)
         }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewConversation() {
-    ComposeTutorialTheme {
-        Conversation(SampleData.conversationSample)
-    }
-}
-
-@Composable
-fun nightModeToggle() {
-    var isNight by remember { mutableStateOf(false) }
-    //sets background colours
-    val backgroundColor by animateColorAsState(
-        if (isNight) MaterialTheme.colorScheme.onBackground else MaterialTheme.colorScheme.surfaceDim
-    )
-    //creates a screensize box with a background color.
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(backgroundColor)
-    ) {
-        //create the conversation
-        PreviewConversation()
-        //create button for night mode
-        Image(
-            painter = painterResource(R.drawable.simple_moon_icon_png),
-            contentDescription = "moon image",
-
-            modifier = Modifier
-                .align(Alignment.TopEnd)
-                .size(35.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
-                .clickable{isNight = !isNight}
-        )
-    }
-}
-
-
-@Preview(name = "Light mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark mode"
-)
-
-@Preview
-@Composable
-fun PreviewMessageCard() {
-    ComposeTutorialTheme {
-        Surface {
-            MessageCard(
-                msg = Message("mina", "smth")
-            )
+        composable("Conversation_Screen") {
+            ConversationScreen(navController)
         }
-    }
+    })
 }
+
 
 
 
